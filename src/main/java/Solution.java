561e0b5eb7fb86e4c5377b517c3776a89d5bd060
tatianastaicova <staicova.tatiana@gmail.com>
2021-11-18 18:36:38 -0500

    Lab done!

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -9,6 +9,12 @@ public class Solution {
 
     public int alternatingCharacters(String s) {
         // Write your code here
-        return 0;
+        int count = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
